3ce597001567d91758041b08dae7686e360d40df
alexander2604gg <110357743+alexander2604gg@users.noreply.github.com>
2025-07-08 22:05:31 -0700

    Update ProductFeignClient.java

--- inventory/src/main/java/com/example/inventory/service/client/ProductFeignClient.java
+++ inventory/src/main/java/com/example/inventory/service/client/ProductFeignClient.java
@@ -8,12 +8,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "products", contextId = "productsFeignClient")
+@FeignClient(
+    name = "products",
+    url = "https://order-management-system-bxrc.onrender.com",
+    contextId = "productsFeignClient"
+)
 public interface ProductFeignClient {
 
     @GetMapping(value = "/api/products/search-name/{productId}" )
     ResponseEntity<ProductNameResponseDTO> getNameProductByProductId (@PathVariable Long productId);
+
     @GetMapping("api/products/search/{productId}")
     ResponseEntity<ProductResponseDTO> getProductById (@PathVariable Long productId);
 
 }
+
